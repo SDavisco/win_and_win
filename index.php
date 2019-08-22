86c895537c8b948e6ab0514b15220949d2085771
SDavisco <42480979+SDavisco@users.noreply.github.com>
2019-08-22 16:48:10 -0500

    actulizando archivos

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+    </head>
+    <body>
+        <section>
+            <div id="section">
+                
+            </div>
+        </section>
+    </body>
+</html>
