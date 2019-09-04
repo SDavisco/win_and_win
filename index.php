f89cf6fec08e4382780d80787dbcb2545cd0ae68
SDavisco <42480979+SDavisco@users.noreply.github.com>
2019-09-04 17:34:19 -0500

    index definido
    mejorando ando

--- index.php
+++ index.php
@@ -6,9 +6,14 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
     </head>
     <body>
+        <?php include 'iheader.php';?>    
         <section>
             <div id="section">
-                
+            <p>Casas de playa o de campo, terrenos agricolas o indistriales, departamentos,
+            locales comercial y mas propiedades.</p>
+            </div>
+            <div id="section">
+            <p>Sacale el maximo provecho a tu inmueble a traves de nuestras alianzas estrategicas.</p>
             </div>
         </section>
     </body>
