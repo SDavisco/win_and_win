a45cb6cedec31532b60941862e60fd4fa95080b9
SDavisco <42480979+SDavisco@users.noreply.github.com>
2019-09-07 11:21:26 -0500

    actulizacion

--- index.php
+++ index.php
@@ -9,11 +9,18 @@
         <?php include 'layout/header.php';?>    
         <section>
             <div id="section">
-            <p>Casas de playa o de campo, terrenos agricolas o indistriales, departamentos,
-            locales comercial y mas propiedades.</p>
+                <div class="titulos">
+                    Venta de Propiedades
+                </div>
+                <p>Casas de playa o de campo, terrenos agricolas o indistriales, departamentos,
+                locales comercial y mas propiedades.</p>
+                <button>más información...</button>
             </div>
             <div id="section">
-            <p>Sacale el maximo provecho a tu inmueble a traves de nuestras alianzas estrategicas.</p>
+                    <div class="titulos">
+                            Venda su Propiedad
+                    </div>
+                <p>Sacale el maximo provecho a tu inmueble a traves de nuestras alianzas estrategicas.</p>
             </div>
         </section>
     </body>
