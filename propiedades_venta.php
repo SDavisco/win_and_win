bf50c26ae23a2a22c301e73891e7547e9cd97632
SDavisco <sistemasweb@davisco.com.pe>
2019-09-11 11:20:35 -0500

    gg

--- propiedades_venta.php
+++ propiedades_venta.php
@@ -6,6 +6,21 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
     </head>
     <body>
-        
+        <?php include 'layout/header.php';?>
+        <div class="section">
+            Ya sea que estes buscando tu primer departamento, casa, local comercial,
+            vivienda fusionada con local comercial , terreno industrial, deposito, 
+            o casa de playa/ campo, te ayudamos a buscarla de acuerdo a tus necesidades
+            y te asesoramos en el proceso de analisis comparativo de precios del mercado,
+            negociación con el propietario y el tramite legal para garantizar una compra
+            100% segura y eficiente.
+            <ul>
+            <li>Departamentos</li>
+            <li>Casa de Playa</li>
+            <li>Casa de Campo</li>
+            <li>Terrenos</li>
+            <li>Locales Comerciales</li>
+            </ul>
+        </div>
     </body>
 </html>
