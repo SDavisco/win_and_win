3524c533aff0ed2c971b7e6c1baa413000479d64
SDavisco <sistemasweb@davisco.com.pe>
2019-09-19 16:52:15 -0500

    c

--- inversion_inmobiliaria.php
+++ inversion_inmobiliaria.php
@@ -6,6 +6,25 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
     </head>
     <body>
-        
+        <div class="Informacion">
+
+        </div>
+        <div class="datos">
+
+        </div>
+        <form action="enviar.php" method="post">
+				<div class="row">
+				<input type="text" name="nombre" placeholder="Nombre" class="caja-1" required>
+				</div>
+				<div class="row">
+				<input type="email" name="email" placeholder="Email" class="caja-1" required>
+				</div>
+				<div class="row">
+				<input type="number" name="telefono" placeholder="Teléfono" class="caja-1" required>
+				</div>
+				<textarea name="texto" id="" cols="25" rows="10" class="caja-2" placeholder="Deja tu mensaje" required>				
+					</textarea>
+				<button type="submit" class="b_correo">Enviar</button>
+		</form>
     </body>
 </html>
--- venda_prop.php
+++ venda_prop.php
@@ -4,8 +4,40 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+    <script>
+        function evalSlider()
+        {
+            var sliderValue = document.getElementById('urgencia').value;
+            document.getElementById('sliderVal').innerHTML = sliderValue;
+        }
+    </script>
     </head>
     <body>
-        
+    <?php include 'layout/header.php';?>
+    <div>
+        <form action="#" method="POST">
+            <div class="campo">
+                <label>Nombre:</label>
+                <input type="text" name="nombre" id="nombre">
+            </div>
+            <div class="campo">
+                <label>Telefono:</label>
+                <input type="tel" name="telefono" id="telefono">
+            </div>
+            <div class="campo">
+                <label>Ciudad:</label>
+                <input type="text" name="ciudad" id="ciudad">
+            </div>
+            <div class="campo">
+                <label>Distrito:</label>
+                <input type="text" name="distrito" id="distrito">
+            </div>
+                <label>Nivel de urgencia</label>
+                <input type="range" name="urgencia" id="urgencia" min="1" max="10" onchange="evalSlider()">
+                <output id="sliderVal"></output>
+            <div class="campo"></div>
+            
+        </form>
+    </div>
     </body>
 </html>
