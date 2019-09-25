8ea593aff7e6256288796195a36d84eba8701f60
SDavisco <sistemasweb@davisco.com.pe>
2019-09-25 17:49:08 -0500

    plus

--- inversion_inmobiliaria.php
+++ inversion_inmobiliaria.php
@@ -10,22 +10,32 @@
         <div class="Informacion">
 
         </div>
-        <div class="datos">
+        <div class="info">
 
-        </div>
+		</div>
+		<section id="venta">
         <form action="enviar.php" method="post">
-				<div class="row">
+				<div class="campo">
+					<label>Nombre y Apellido</label><br>
 				<input type="text" name="nombre" placeholder="Nombre" class="caja-1" required>
 				</div>
-				<div class="row">
+				<div class="campo">
+					<label>Correo electronico</label><br>
 				<input type="email" name="email" placeholder="Email" class="caja-1" required>
 				</div>
-				<div class="row">
+				<div class="campo">
+					<label>Telefono</label><br>
 				<input type="number" name="telefono" placeholder="Teléfono" class="caja-1" required>
 				</div>
+				<div class="campo">
+					<label>Mensaje:</label><br>
 				<textarea name="texto" id="" cols="25" rows="10" class="caja-2" placeholder="Deja tu mensaje" required>				
 					</textarea>
+				</div>
+				<div class="campo">
 				<button type="submit" class="b_correo">Enviar</button>
+			    </div>
 		</form>
+		</section>
     </body>
 </html>
--- venda_prop.php
+++ venda_prop.php
@@ -3,7 +3,7 @@
     <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
     <script>
         function evalSlider()
         {
@@ -14,30 +14,38 @@
     </head>
     <body>
     <?php include 'layout/header.php';?>
-    <div>
+    <section id="venta">
+        <div id="info">
+                <p>Una mañana, tras un sueño intranquilo, Gregorio Samsa se despertó convertido en un monstruoso insecto. 
+                Estaba echado de espaldas sobre un duro caparazón y, al alzar la cabeza, vio su vientre convexo y oscuro,
+                surcado por curvadas callosidades, sobre el que casi no se aguantaba la colcha, que estaba a</p>
+        </div>  
         <form action="#" method="POST">
             <div class="campo">
-                <label>Nombre:</label>
+                <label>Nombre:</label><br>
                 <input type="text" name="nombre" id="nombre">
             </div>
             <div class="campo">
-                <label>Telefono:</label>
+                <label>Telefono:</label><br>
                 <input type="tel" name="telefono" id="telefono">
             </div>
             <div class="campo">
-                <label>Ciudad:</label>
+                <label>Ciudad:</label><br>
                 <input type="text" name="ciudad" id="ciudad">
             </div>
             <div class="campo">
-                <label>Distrito:</label>
+                <label>Distrito:</label><br>
                 <input type="text" name="distrito" id="distrito">
             </div>
-                <label>Nivel de urgencia</label>
+            <div class="campo">
+                <label>Nivel de urgencia</label><br>
                 <input type="range" name="urgencia" id="urgencia" min="1" max="10" onchange="evalSlider()">
                 <output id="sliderVal"></output>
-            <div class="campo"></div>
-            
+            </div>
+            <div class="campo">
+                <input type="submit" name="boton" value="enviar    " id="boton">
+                </div>
         </form>
-    </div>
+    </section>
     </body>
 </html>
